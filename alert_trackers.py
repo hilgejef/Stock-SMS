bc445007785c804773e2575bc1c96c736b64ddfb
Hilger <jeff@hilger.me>
2016-05-15 05:40:14 +0000

    Reduced db calls to 1 in alert_trackers.py with ORDER BY

--- alert_trackers.py
+++ alert_trackers.py
@@ -8,7 +8,7 @@ from urlparse import urlparse
 from twilio.rest import TwilioRestClient
 from twilio.rest.resources import Connection
 from twilio.rest.resources.connection import PROXY_TYPE_HTTP
-from stock_sms import Tracker, db
+from stock_sms import Tracker
 from strings import tracker_alert
 from request_stock_info import get_all_symbol
 
@@ -21,21 +21,22 @@ proxy_url = os.environ.get("http_proxy")
 host, port = urlparse(proxy_url).netloc.split(":")
 Connection.set_proxy_info(host, int(port), proxy_type=PROXY_TYPE_HTTP)
 
-# Find unique symbols, make Yahoo Finance API call for each symbol,
-# send expanded info to each phone number subscribed to that symbol
-for symbol_tuple in db.session.query(Tracker.symbol).distinct().all():
-    symbol = symbol_tuple[0]
-    info = get_all_symbol(symbol)
+# Create map of symbol : [phone numbers] using ORDER BY call to Tracker
+# database, then send
+current_symbol = None
+for tracker in Tracker.query.order_by("symbol"):
+    if tracker.symbol != current_symbol:
+        info = get_all_symbol(tracker.symbol)
+        current_symbol = tracker.symbol
 
-    for tracker in Tracker.query.filter_by(symbol=symbol).all():
-        body = tracker_alert.format(symbol=symbol,
-                                    last_trade_price=info["last_trade_price"],
-                                    opening_price=info["today_open"],
-                                    closing_price=info["previous_close"],
-                                    price_high=info["todays_high"],
-                                    price_low=info["todays_low"],
-                                    last_trade_time=["last_trade_time"])
+    body = tracker_alert.format(symbol=current_symbol,
+                                last_trade_price=info["last_trade_price"],
+                                opening_price=info["today_open"],
+                                closing_price=info["previous_close"],
+                                price_high=info["todays_high"],
+                                price_low=info["todays_low"],
+                                last_trade_time=["last_trade_time"])
 
-        client.messages.create(to=tracker.phone_number,
-                              from_=Twilio_Credentials.phone_number,
-                              body=body)
+    client.messages.create(to=tracker.phone_number,
+                           from_=Twilio_Credentials.phone_number,
+                           body=body)
